41223ac495f1620e039443ed3bc8e506d559b17c
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-17 02:51:05 +0100

    WIP: fixed CORS issue

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const CoursesRouter = require('./courses/course-router.js')
 
 const server = express()
 
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
+
 server.use(express.json())
 
 server.get('/', (req, res) => {
